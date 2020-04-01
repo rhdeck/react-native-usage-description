2c36a4853b07204644f64181d359b9eb8057086f
Ray Deck <ray@raydeck.com>
2020-03-31 19:04:06 -0500

    Handling all possible usage descriptions in IOS

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # react-native-usage-description
- Manage usage text for react-native apps to be deployed in IOS
+
+Manage usage text for react-native apps to be deployed in IOS
+
+If you use certain sensors and subsystems on iOS, your app will crash if you do not have the usage description set to ask the user for permission to use the system. This plugin allows you to set that text easily from the react-native command line without ever opening Xcode.
+
+## installation
+
+```bash
+yarn add -D react-native-usage-description
+#or
+npm install --save-dev react-native-usage-description
+```
+
+## usage
+
+This adds a series of options to the react-native cli
+
+**Note** that the new description is optional for all utilities, allowing the default text to be applied. This is handy for quick development, but you will want to replace the text with something specific to your app before submitting to the app store.
+
+```bash
+$ react-native --help
+...
+  set-photo-library-add-description [descriptionText]         Set Photo Library Add Usage Description (leave blank for default: "This app adds photos to the user's photo library")
+  set-photo-library-description [descriptionText]             Set Photo Library Usage Description (leave blank for default: "This app accesses the user's photo library")
+  set-camera-description [descriptionText]                    Set Camera Usage Description (leave blank for default: "This app uses the device camera")
+  set-location-always-description [descriptionText]           Set Location Always Usage Description (leave blank for default: "This app uses location services all the time")
+  set-location-when-in-use-description [descriptionText]      Set Location When In Use Usage Description (leave blank for default: "This app uses location services only when the app is running")
+  set-contacts-description [descriptionText]                  Set Contacts Usage Description (leave blank for default: "This app uses the address book")
+  set-calendars-description [descriptionText]                 Set Calendars Usage Description (leave blank for default: "This app uses or modifies the user's calendar information")
+  set-reminders-description [descriptionText]                 Set Reminders Usage Description (leave blank for default: "This app creates reminders in the Reminders app")
+  set-health-share-description [descriptionText]              Set Health Share Usage Description (leave blank for default: "This app uses data from the Health app")
+  set-health-update-description [descriptionText]             Set Health Update Usage Description (leave blank for default: "This app provides health information to the Health app")
+  set-nfcreader-description [descriptionText]                 Set NFCReader Usage Description (leave blank for default: "This app uses the NFC reader")
+  set-bluetooth-peripheral-description [descriptionText]      Set Bluetooth Peripheral Usage Description (leave blank for default: "This app works with Bluetooth devices")
+  set-microphone-description [descriptionText]                Set Microphone Usage Description (leave blank for default: "This app uses the device microphone")
+  set-siri-description [descriptionText]                      Set Siri Usage Description (leave blank for default: "This app provides a SiriKit Intent")
+  set-speech-recognition-description [descriptionText]        Set Speech Recognition Usage Description (leave blank for default: "This app uses speech recognition")
+  set-motion-description [descriptionText]                    Set Motion Usage Description (leave blank for default: "This app uses the device motion tracking hardware")
+  set-video-subscriber-account-description [descriptionText]  Set Video Subscriber Account Usage Description (leave blank for default: "(tvOS only) This app uses the video subscriber account")
+  set-apple-music-description [descriptionText]               Set Apple Music Usage Description (leave blank for default: "This app uses Apple Music integration")
+  set-face-idusage-description [descriptionText]              Set Face IDUsage Description (leave blank for default: "This app uses FaceID")
+  set-all-usage-descriptions                                  Set all usage descriptions to the default text in this package
+```
+
+Note this will apply all descriptions as defaults.
--- react-native.config.js
+++ react-native.config.js
@@ -1,27 +1,137 @@
 const { resolve, join } = require("path");
-const { existsSync, readFileSync, readdirSync } = require("fs");
+const { existsSync, readFileSync, readdirSync, writeFileSync } = require("fs");
 const plist = require("plist");
+const { sync } = require("glob");
+var iosPath = resolve(process.cwd(), "ios");
+const addText = (key, value) => {
+  if (!existsSync(iosPath)) {
+    console.error("Could not find ios in ", thisPath, iosPath);
+    console.log(readdirSync(thisPath));
+    return false;
+  }
+  const plists = sync(resolve(join(iosPath, "*", "Info.plist")));
+  plists.map(path => {
+    const source = readFileSync(path, "utf8");
+    var o = plist.parse(source);
+    o[key] = value;
+    const xml = plist.build(o);
+    writeFileSync(path, xml);
+  });
+};
+
+const a = [
+  {
+    key: "NSPhotoLibraryAddUsageDescription",
+    text: "This app adds photos to the user's photo library"
+  },
+  {
+    key: "NSPhotoLibraryUsageDescription",
+    text: "This app accesses the user's photo library"
+  },
+  { key: "NSCameraUsageDescription", text: "This app uses the device camera" },
+  {
+    key: "NSLocationAlwaysUsageDescription",
+    text: "This app uses location services all the time"
+  },
+  {
+    key: "NSLocationWhenInUseUsageDescription",
+    text: "This app uses location services only when the app is running"
+  },
+  { key: "NSContactsUsageDescription", text: "This app uses the address book" },
+  {
+    key: "NSCalendarsUsageDescription",
+    text: "This app uses or modifies the user's calendar information"
+  },
+  {
+    key: "NSRemindersUsageDescription",
+    text: "This app creates reminders in the Reminders app"
+  },
+  {
+    key: "NSHealthShareUsageDescription",
+    text: "This app uses data from the Health app"
+  },
+  {
+    key: "NSHealthUpdateUsageDescription",
+    text: "This app provides health information to the Health app"
+  },
+  { key: "NFCReaderUsageDescription", text: "This app uses the NFC reader" },
+  {
+    key: "NSBluetoothPeripheralUsageDescription",
+    text: "This app works with Bluetooth devices"
+  },
+  {
+    key: "NSMicrophoneUsageDescription",
+    text: "This app uses the device microphone"
+  },
+  { key: "NSSiriUsageDescription", text: "This app provides a SiriKit Intent" },
+  {
+    key: "NSSpeechRecognitionUsageDescription",
+    text: "This app uses speech recognition"
+  },
+  {
+    key: "NSMotionUsageDescription",
+    text: "This app uses the device motion tracking hardware"
+  },
+  {
+    key: "NSVideoSubscriberAccountUsageDescription",
+    text: "(tvOS only) This app uses the video subscriber account"
+  },
+  {
+    key: "NSAppleMusicUsageDescription",
+    text: "This app uses Apple Music integration"
+  },
+  { key: "NSFaceIDUsageDescription", text: "This app uses FaceID" }
+];
+const commands = a.map(({ key, text }) => {
+  const names = (key.startsWith("NS") ? key.substr(2) : key).replace(
+    /([a-z])([A-Z])/g,
+    "$1 $2"
+  );
+  const keyName = names
+    .toLowerCase()
+    .split(" ")
+    .filter(w => w !== "usage")
+    .join("-");
+  const name = "set-" + keyName + " [descriptionText]";
+  const description =
+    "Set " + names + ' (leave blank for default: "' + text + '")';
+  return {
+    name,
+    description,
+    func: ([desc]) => {
+      if (!desc) {
+        console.warn(
+          "This will use the default text - it may not pass app store check"
+        );
+        desc = text;
+      }
+      addText(key, desc);
+    }
+  };
+});
+
+module.exports = {
+  commands: [
+    ...commands,
+    {
+      name: "set-all-usage-descriptions",
+      description:
+        "Set all usage descriptions to the default text in this package",
+      func: () => {
+        a.forEach(({ key, text }) => {
+          addText(key, text);
+        });
+      }
+    }
+  ]
+};
+/*
 const defaultPhotoDescription = "This app requires the photo library";
 const defaultPhotoAddDescription = "This app must add to the photo library";
 const defaultCameraDescription = "This app requires the camera";
 const defaultMicrophoneDescription = "This app requires the microphone";
 const defaultFaceIDDescription = "This app requires FaceID";
 const defaultCalendarDescription = "This app requires access to the calendar";
-var iosPath = resolve(process.cwd(), "ios");
-const addText = (key, value) => {
-if (!existsSync(iosPath)) {
-  console.error("Could not find ios in ", thisPath, iosPath);
-  console.log(readdirSync(thisPath));
-  return false
-}
-const plists = glob.sync(resolve(join(iosPath, "*", "Info.plist")));
-plists.map(path => {
-  const source = readFileSync(path, "utf8");
-  var o = plist.parse(source);
-  o[key] = value;
-  const xml = plist.build(o);
-  writeFileSync(path, xml);
-});
 
 module.exports = {
   commands: [
@@ -33,59 +143,83 @@ module.exports = {
           console.warn("This default text may get rejected by the appStore");
           description = defaultPhotoDescription;
         }
-        return addText("NSPhotoLibraryUsageDescription", description)
+        return addText("NSPhotoLibraryUsageDescription", description);
       }
-    },{
-        name: "set-photo-add-description [newdescription]",
-        description: "set Photo Library Usage Description for write (e.g. adding) access",
-        func: ([description]) => {
-          if (!description) {
-            console.warn("This default text may get rejected by the appStore");
-            description = defaultPhotoAddDescription;
-          }
-          return addText("NSPhotoLibraryAddUsageDescription", description)
+    },
+    {
+      name: "set-photo-add-description [newdescription]",
+      description:
+        "set Photo Library Usage Description for write (e.g. adding) access",
+      func: ([description]) => {
+        if (!description) {
+          console.warn("This default text may get rejected by the appStore");
+          description = defaultPhotoAddDescription;
         }
+        return addText("NSPhotoLibraryAddUsageDescription", description);
       }
-      ,{
-        name: "set-camera-description [newdescription]",
-        description: "set Camera Usage Description for write (e.g. adding) access",
-        func: ([description]) => {
-          if (!description) {
-            console.warn("This default text may get rejected by the appStore");
-            description = defaultCameraDescription;
-          }
-          return addText("NSCameraUsageDescription", description)
+    },
+    {
+      name: "set-camera-description [newdescription]",
+      description:
+        "set Camera Usage Description for write (e.g. adding) access",
+      func: ([description]) => {
+        if (!description) {
+          console.warn("This default text may get rejected by the appStore");
+          description = defaultCameraDescription;
         }
-      },{
-        name: "set-microphone-description [newdescription]",
-        description: "set Microphone Usage Description for write (e.g. adding) access",
-        func: ([description]) => {
-          if (!description) {
-            console.warn("This default text may get rejected by the appStore");
-            description = defaultMicrophoneDescription;
-          }
-          return addText("NSMicrophoneUsageDescription", description)
-        }
-      },{
-        name: "set-faceid-description [newdescription]",
-        description: "set FaceID Usage Description for write (e.g. adding) access",
-        func: ([description]) => {
-          if (!description) {
-            console.warn("This default text may get rejected by the appStore");
-            description = defaultFaceIDDescription;
-          }
-          return addText("NSFaceIDUsageDescription", description)
-        }
-      },{
-        name: "set-calendar-description [newdescription]",
-        description: "set Calendar Usage Description for write (e.g. adding) access",
-        func: ([description]) => {
-          if (!description) {
-            console.warn("This default text may get rejected by the appStore");
-            description = defaultCalendarDescription;
-          }
-          return addText("NSCalendarUsageDescription", description)
-        }
+        return addText("NSCameraUsageDescription", description);
       }
+    },
+    {
+      name: "set-microphone-description [newdescription]",
+      description:
+        "set Microphone Usage Description for write (e.g. adding) access",
+      func: ([description]) => {
+        if (!description) {
+          console.warn("This default text may get rejected by the appStore");
+          description = defaultMicrophoneDescription;
+        }
+        return addText("NSMicrophoneUsageDescription", description);
+      }
+    },
+    {
+      name: "set-faceid-description [newdescription]",
+      description:
+        "set FaceID Usage Description for write (e.g. adding) access",
+      func: ([description]) => {
+        if (!description) {
+          console.warn("This default text may get rejected by the appStore");
+          description = defaultFaceIDDescription;
+        }
+        return addText("NSFaceIDUsageDescription", description);
+      }
+    },
+    {
+      name: "set-calendar-description [newdescription]",
+      description:
+        "set Calendar Usage Description for write (e.g. adding) access",
+      func: ([description]) => {
+        if (!description) {
+          console.warn("This default text may get rejected by the appStore");
+          description = defaultCalendarDescription;
+        }
+        return addText("NSCalendarUsageDescription", description);
+      }
+    },
+    {
+      name: "set-all-descriptions",
+      description:
+        "Set all descriptions to the default text. Only really useful for tools that you will never deploy",
+      func: () => {
+        addText("NSCalendarUsageDescription", defaultCalendarDescription);
+        addText("NSCameraUsageDescription", defaultCameraDescription);
+        addText("NSFaceIDUsageDescription", defaultFaceIDDescription);
+        addText("NSMicrophoneUsageDescription", defaultMicrophoneDescription);
+        addText("NSPhotoLibraryUsageDescription", defaultPhotoDescription);
+        addText("NSPhotoLibraryAddUsageDescription", defaultPhotoDescription);
+      }
+    }
   ]
 };
+
+*/
